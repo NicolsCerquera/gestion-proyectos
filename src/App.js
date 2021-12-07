2f01cf7a0f524c9bb1cabf10920021eab000aa92
NicolsCerquera <cerqueras@uninorte.edu.co>
2021-12-06 23:53:46 -0500

    Aplicacion con login- arreglar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 /* import "./Components/Navbar.css"; */
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Users from "./pages/Users";
+import Login from "./components/Login.js";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+      </Routes>
+
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/home" element={<Home />}></Route>
         <Route exact path="/users" element={<Users />}></Route>
         <Route exact path="/projects" element={<Projects />}></Route>
       </Routes>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "../src/Components/Navbar.css";
+import "./components/Navbar.css";
 import App from "./App";
 import * as bootstrap from "bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
